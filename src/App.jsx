0fb3b0f2c8f009577db45ad863dd58119ca4ef93
VR-vranjan <vranjan00700@gmail.com>
2026-01-22 19:17:27 +0530

    Added App.jsx with routing for Home and BookDetails

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,16 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import BookDetails from "./pages/BookDetails";
-import Navbar from "./components/Navbar";
-import { useState } from "react";
-import './App.css';
+import "./App.css";
 
-
-function App() {
-  const [books, setBooks] = useState([
-    {
-      id: 1,
-      title: "Atomic Habits",
-      author: "James Clear",
-      email: "publisher@books.com",
-      age: 5,
-      publisher: "Penguin",
-      publishedDate: "2018",
-      overview: "A practical guide to building good habits."
-    }
-  ]);
-
+export default function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Home books={books} setBooks={setBooks} />} />
-        <Route path="/book/:id" element={<BookDetails books={books} />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/book/:id" element={<BookDetails />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
-export default App;
