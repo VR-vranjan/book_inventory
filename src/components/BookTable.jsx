30b4a824785df610c0073951a6157fc6a836a7d4
VR-vranjan <vranjan00700@gmail.com>
2026-01-22 18:50:03 +0530

    Implemented book table with scroll and delete functionality

--- src/components/BookTable.jsx
+++ src/components/BookTable.jsx
@@ -1,35 +1,55 @@
 import { Link } from "react-router-dom";
 
 export default function BookTable({ books, setBooks }) {
+  // Delete book
   const deleteBook = (id) => {
-    setBooks(books.filter(book => book.id !== id));
+    if (window.confirm("Are you sure you want to delete this book?")) {
+      setBooks(books.filter(book => book.id !== id));
+    }
   };
 
   return (
     <div style={{ maxHeight: "300px", overflowY: "scroll", marginTop: "20px" }}>
-      <table border="1" width="100%">
+      <table
+        style={{
+          width: "100%",
+          borderCollapse: "collapse",
+          textAlign: "left"
+        }}
+      >
         <thead>
-          <tr>
+          <tr style={{ background: "#ddd" }}>
             <th>Title</th>
             <th>Author</th>
             <th>Email</th>
+            <th>Publisher</th>
+            <th>Age</th>
             <th>Actions</th>
           </tr>
         </thead>
-
         <tbody>
           {books.map(book => (
-            <tr key={book.id}>
+            <tr key={book.id} style={{ borderBottom: "1px solid #ccc" }}>
               <td>{book.title}</td>
               <td>{book.author}</td>
               <td>{book.email}</td>
+              <td>{book.publisher}</td>
+              <td>{book.age}</td>
               <td>
-                <Link to={`/book/${book.id}`}>View</Link>
-                {" | "}
+                <Link to={`/book/${book.id}`} style={{ marginRight: "10px" }}>
+                  View
+                </Link>
                 <button onClick={() => deleteBook(book.id)}>Delete</button>
               </td>
             </tr>
           ))}
+          {books.length === 0 && (
+            <tr>
+              <td colSpan="6" style={{ textAlign: "center" }}>
+                No books available
+              </td>
+            </tr>
+          )}
         </tbody>
       </table>
     </div>
